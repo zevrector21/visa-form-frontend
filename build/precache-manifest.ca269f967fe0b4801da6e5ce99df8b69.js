172589398118b3933dfc6c3c05cc09ca0c125158
zevrector21 <zevrector21@gmail.com>
2019-09-11 09:43:30 -0300

    build added

---
+++ build/precache-manifest.ca269f967fe0b4801da6e5ce99df8b69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5f019b96abe4480e9d5a",
+    "url": "/static/js/main.5f019b96.chunk.js"
+  },
+  {
+    "revision": "fa4105519ee70eddc558",
+    "url": "/static/js/1.fa410551.chunk.js"
+  },
+  {
+    "revision": "5f019b96abe4480e9d5a",
+    "url": "/static/css/main.63daf8fb.chunk.css"
+  },
+  {
+    "revision": "fa4105519ee70eddc558",
+    "url": "/static/css/1.3ef5a839.chunk.css"
+  },
+  {
+    "revision": "81611ae4cf290dac435f1b55c8ef3cf9",
+    "url": "/index.html"
+  }
+];
