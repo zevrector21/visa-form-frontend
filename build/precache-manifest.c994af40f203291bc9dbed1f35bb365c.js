09d5b4b5f2fe213c4663d9cc3c229085970b7f3a
zevrector21 <zevrector21@gmail.com>
2019-10-01 15:31:18 -0300

    IP change

--- build/precache-manifest.c994af40f203291bc9dbed1f35bb365c.js
+++ build/precache-manifest.c994af40f203291bc9dbed1f35bb365c.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "cb53b6977d5817101b09",
-    "url": "/static/js/main.cb53b697.chunk.js"
+    "revision": "db8b92d16263f4ec9f17",
+    "url": "/static/js/main.db8b92d1.chunk.js"
   },
   {
     "revision": "f8aa17a1fbc8837676ec",
     "url": "/static/js/1.f8aa17a1.chunk.js"
   },
   {
-    "revision": "cb53b6977d5817101b09",
+    "revision": "db8b92d16263f4ec9f17",
     "url": "/static/css/main.2c8d1c8a.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/static/css/1.c2d74f27.chunk.css"
   },
   {
-    "revision": "b763d9705603bf2dddd25b0a72068e30",
+    "revision": "490388cf97db39a9ba9f404591483886",
     "url": "/index.html"
   }
 ];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1d2178cc41f99f890fb1ab96e49df0cc.js"
+  "/precache-manifest.c994af40f203291bc9dbed1f35bb365c.js"
 );
 
 workbox.clientsClaim();
