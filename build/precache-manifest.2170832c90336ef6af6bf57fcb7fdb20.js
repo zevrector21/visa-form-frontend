80787714238280586c8f6fdbca8973015bc0b852
zevrector21 <zevrector21@gmail.com>
2019-10-18 15:09:19 -0300

    push again.

---
+++ build/precache-manifest.2170832c90336ef6af6bf57fcb7fdb20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4fbdba97ac770e8d0ee0",
+    "url": "/static/js/main.4fbdba97.chunk.js"
+  },
+  {
+    "revision": "8e1477c77e2dd5c26993",
+    "url": "/static/js/1.8e1477c7.chunk.js"
+  },
+  {
+    "revision": "4fbdba97ac770e8d0ee0",
+    "url": "/static/css/main.00a4d27a.chunk.css"
+  },
+  {
+    "revision": "8e1477c77e2dd5c26993",
+    "url": "/static/css/1.6f72d7db.chunk.css"
+  },
+  {
+    "revision": "0ef7e0bd3635e2388a5857a8d7f01279",
+    "url": "/index.html"
+  }
+];
