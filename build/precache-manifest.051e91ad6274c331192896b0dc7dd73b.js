a3812726ede25d0bf081581bd6d01ea66f4b9db4
zevrector21 <zevrector21@gmail.com>
2020-02-10 00:03:04 -0300

    Temporary update ip

--- build/precache-manifest.051e91ad6274c331192896b0dc7dd73b.js
+++ build/precache-manifest.051e91ad6274c331192896b0dc7dd73b.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "821cc7a37b9e068f86a7",
+    "revision": "4f7cd0f3a0ed461ca253",
     "url": "/static/css/main.050f5c82.chunk.css"
   },
   {
-    "revision": "821cc7a37b9e068f86a7",
-    "url": "/static/js/main.821cc7a3.chunk.js"
+    "revision": "4f7cd0f3a0ed461ca253",
+    "url": "/static/js/main.4f7cd0f3.chunk.js"
   },
   {
     "revision": "b4841f014ea0123edb9b",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "d1c15577643c60002c4a8cb730018654",
+    "revision": "47c7d3c35a0915935b57c6b013e4e9eb",
     "url": "/index.html"
   }
 ];
