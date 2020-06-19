48c39d1a5a6c1ba4e78524a5dcc48627ccbb80e7
zevrector21 <zevrector21@gmail.com>
2020-06-19 13:14:57 -0300

    build uploaded

---
+++ build/precache-manifest.893cc9093edb61bd6f531ed739f0cab5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa74f5e7fa6e9ff8a5ea",
+    "url": "/static/css/main.3f502d05.chunk.css"
+  },
+  {
+    "revision": "aa74f5e7fa6e9ff8a5ea",
+    "url": "/static/js/main.aa74f5e7.chunk.js"
+  },
+  {
+    "revision": "816cdf1b0896834aae61",
+    "url": "/static/css/1.4563fff1.chunk.css"
+  },
+  {
+    "revision": "816cdf1b0896834aae61",
+    "url": "/static/js/1.816cdf1b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "db6a9558f0288663ba35989aa83110fd",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.18610bdfa1e5a1b32b915191abc23648.js"
+  "/precache-manifest.893cc9093edb61bd6f531ed739f0cab5.js"
 );
 
 workbox.clientsClaim();
